ca325040dbd357e2acd335b1e102e7f038617577
ghersmann <gustav@gustavhersmann.com>
2024-02-28 16:29:31 +0100

    Finished kata no. 2 - Extract Class Name

--- 02-extract-class-name/extract-class-name.js
+++ 02-extract-class-name/extract-class-name.js
@@ -1,3 +1,45 @@
 function extractClassName(sessionTitle) {
-  // Add you solution here
+  let arr = sessionTitle.toLowerCase().split(" ");
+  let howLong = arr.length;
+  let year = 0;
+  let month = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (howLong < 4) {
+      return null;
+    } else if (Number(arr[i]) > 1900 && Number(arr[i] < 2100)) {
+      year = arr[i];
+    } else if (Number(arr[i] < 1900 || Number(arr[i]) > 2100)) {
+      return null;
+    } else if (arr[i] === "januar") {
+      month = "01";
+    } else if (arr[i] === "februar") {
+      month = "02";
+    } else if (arr[i] === "märz") {
+      month = "03";
+    } else if (arr[i] === "maerz") {
+      month = "03";
+    } else if (arr[i] === "april") {
+      month = "04";
+    } else if (arr[i] === "mai") {
+      month = "05";
+    } else if (arr[i] === "juni") {
+      month = "06";
+    } else if (arr[i] === "juli") {
+      month = "07";
+    } else if (arr[i] === "august") {
+      month = "08";
+    } else if (arr[i] === "september") {
+      month = "09";
+    } else if (arr[i] === "oktober") {
+      month = "10";
+    } else if (arr[i] === "november") {
+      month = "11";
+    } else if (arr[i] === "dezember") {
+      month = "12";
+    }
+  }
+  console.log(year + "-" + month);
+  return year + "-" + month;
 }
+
+// Diese Aufgabe wurde gemeinsam mit Marie und Vanessa gelöst
--- 02-extract-class-name/extract-class-name.test.js
+++ 02-extract-class-name/extract-class-name.test.js
@@ -1,18 +1,26 @@
 describe("Extract Class Name", function () {
   it("should extract correct class name", function () {
-    chai.expect(extractClassName("Live-Session Class 2022 September")).to.equal("2022-09");
+    chai
+      .expect(extractClassName("Live-Session Class 2022 September"))
+      .to.equal("2022-09");
   });
 
   it("should extract correct class name", function () {
-    chai.expect(extractClassName("Live-Session Class 2023 Januar")).to.equal("2023-01");
+    chai
+      .expect(extractClassName("Live-Session Class 2023 Januar"))
+      .to.equal("2023-01");
   });
 
   it("should extract correct class name", function () {
-    chai.expect(extractClassName("Live-Session Class 2022 März")).to.equal("2022-03");
+    chai
+      .expect(extractClassName("Live-Session Class 2022 März"))
+      .to.equal("2022-03");
   });
 
   it("should extract correct class name", function () {
-    chai.expect(extractClassName("Live-Session Class 2022 Maerz")).to.equal("2022-03");
+    chai
+      .expect(extractClassName("Live-Session Class 2022 Maerz"))
+      .to.equal("2022-03");
   });
 
   it("should return null if format does not match", function () {
@@ -20,7 +28,9 @@ describe("Extract Class Name", function () {
   });
 
   it("should return null if format does not match", function () {
-    chai.expect(extractClassName("Live-Session Class 2222222 Juli")).to.equal(null);
+    chai
+      .expect(extractClassName("Live-Session Class 2222222 Juli"))
+      .to.equal(null);
   });
 
   it("should return null if format does not match", function () {
